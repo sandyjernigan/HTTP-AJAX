9a11f4e5aa62406039cbadd805ec71d6deee79dd
sandyjernigan <sandy.jernigan@gmail.com>
2019-06-25 20:27:04 -0400

    created Friendslist file.... attempting to setup connection
    Inside your React application, create a component to display the list of friends coming from the server.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from "axios"
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	state = {
+		friends: []
+	}
+
+	componentDidMount() {
+		axios.get('http://localhost:5000/friends')
+			.then(response => {
+				this.setState({
+					friends: response.data
+				})
+			})
+			.then(() => {
+				return axios.get('http://localhost:5000/')
+			})
+			.then(response => {
+				console.log(response.data)
+			})
+			.catch(err => {
+				console.log('Error:', err)
+			})
+	}
+
+	render() {
+		const { friends } = this.state
+		
+		return (
+			<div className="App">
+        <h1>My App</h1>
+			</div>
+		)
+	}
 }
 
 export default App;

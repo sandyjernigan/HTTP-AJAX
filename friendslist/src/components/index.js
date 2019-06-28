54186b85e4f2be539ece2d3c917c593b66a7b17e
Sandy Jernigan <sandy.jernigan@gmail.com>
2019-06-27 20:17:27 -0400

    Revert "Sandy"

--- friendslist/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Home from "./Home"
-import Friend from "./Friend";
-import Friends from "./Friends";
-import UpdateFriend from "./UpdateFriend";
-
-export { Home, Friend, Friends, UpdateFriend }
